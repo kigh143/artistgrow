25a9d54a815178917e0470f29189914767818ca0
kigh143 <ghkatende@gmail.com>
2021-09-18 22:27:24 +0300

    Added needed pages

---
+++ src/pages/profile.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class profile extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Profile</h2>
+            </div>
+        )
+    }
+}
+
+export default profile;
